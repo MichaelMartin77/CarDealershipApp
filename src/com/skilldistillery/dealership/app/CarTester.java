84bf3f80456b21b2d2cdf507bef59ac9c215d9c2
Michael <martin.michael1477@gmail.com>
2024-09-15 23:30:01 -0500

    first part of E completed

--- src/com/skilldistillery/dealership/app/CarTester.java
+++ src/com/skilldistillery/dealership/app/CarTester.java
@@ -4,33 +4,33 @@ import com.skilldistillery.dealership.entities.Car;
 
 public class CarTester {
 
-  public static void main(String[] args) {
-    // Create two Cars and assign fields
-    Car fredsCar = new Car("Kia", "Sportage", "Hot Pink", 28565);
-    // added setters as well as per instructions from part b of D 
+	public static void main(String[] args) {
+		// Create two Cars and assign fields
+		Car fredsCar = new Car("Kia", "Sportage", "Hot Pink", 28565);
+// 	  fredsCar fields are already set via the constructor, so the setters are not needed as per instructions from part b of D 
 //    fredsCar.setMake("Kia");
 //    fredsCar.setModel("Sportage");
 //    fredsCar.setColor("Hot Pink");
 //    fredsCar.setPurchasePrice(28565);
 
-    Car bernicesCar = new Car();
+		Car bernicesCar = new Car();
 
-    bernicesCar.setMake("Toyota");
-    bernicesCar.setModel("Prius");
-    bernicesCar.setColor("Neon");
-    bernicesCar.setPurchasePrice(32199);
+		bernicesCar.setMake("Toyota");
+		bernicesCar.setModel("Prius");
+		bernicesCar.setColor("Neon");
+		bernicesCar.setPurchasePrice(32199);
 
-    // Have the cars display themselves
-    fredsCar.displayCar();
-    bernicesCar.displayCar();
-    System.out.println();
+		// Have the cars display themselves
+		fredsCar.displayCar();
+		bernicesCar.displayCar();
+		System.out.println();
 
-    String fredCarData = fredsCar.toString();
-    String bernicesCarData = bernicesCar.toString();
+		String fredCarData = fredsCar.toString();
+		String bernicesCarData = bernicesCar.toString();
 
-    // Get the cars' data and display that
-    System.out.println("Fred car data: " + fredCarData);
-    System.out.println("Bernices Car data: " + bernicesCarData);
-  }
+		// Get the cars' data and display that
+		System.out.println("Fred car data: " + fredCarData);
+		System.out.println("Bernices Car data: " + bernicesCarData);
+	}
 
 }
--- src/com/skilldistillery/dealership/entities/CarLot.java
+++ src/com/skilldistillery/dealership/entities/CarLot.java
@@ -1,5 +1,13 @@
 package com.skilldistillery.dealership.entities;
 
 public class CarLot {
-
+	// create an private field to hold the cars (10)
+	private Car[] car = new Car[10]; 
+	// creating a private field that tracks how many cars are currently parked in the lot
+	private int parkedCars = 0; 
+	
+	// create a no arg ctor 
+	public CarLot() {
+		
+	}
 }
